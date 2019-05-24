fb1e1eaeea57265942603db457b0c6f7458f7f5a
macstudent <singhg1696>
2019-05-24 19:08:03 -0400

    Sum of Array Deduction

--- src/main/java/com/gurwinder/ExampleMain.java
+++ src/main/java/com/gurwinder/ExampleMain.java
@@ -3,6 +3,35 @@ package com.gurwinder;
 public class ExampleMain {
     public static void main(String[] args)
     {
+     int a[]= new int[5];
 
+        int y[]={10,20,30,40,50};
+
+     a[0]=10;
+     a[1]=20;
+     a[2]=30;
+     a[3]=40;
+     a[4]=50;
+
+     for (int i=0;i<a.length;i++){
+         System.out.println("a[] : "+a[i]);
+     }
+        for (int i=0;i<y.length;i++){
+            System.out.println("y[] : "+y[i]);
+        }
+
+        ExampleMain exampleMain= new ExampleMain();
+        int s=exampleMain.arrayAddition(a);
+        System.out.println("TotalSum of array is "+s);
+    }
+
+    public int arrayAddition(int a[]) {
+
+        int sum = 0;
+        for (int x : a)
+        {
+            sum= sum+ x;
+        }
+            return sum;
     }
 }
